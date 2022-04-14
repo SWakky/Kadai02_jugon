52bb8c65c437014c0232282ed81c1fae7906ca10
SWakky <102840595+SWakky@users.noreply.github.com>
2022-04-14 17:44:08 +0900

    バージョンアップしました

--- js/jugon.js
+++ js/jugon.js
@@ -2,7 +2,7 @@
 
 // 呪言スタートボタン
 $("#start-btn").on("click", function () {
-  const random = Math.floor(Math.random() * 2);
+  const random = Math.floor(Math.random() * 3);
   console.log(random, "ランダム数字");
 
   if (random === 0) {
@@ -11,7 +11,10 @@ $("#start-btn").on("click", function () {
   } else if (random === 1) {
     console.log("ジーッ");
     $("div").children("img").attr("src", "image/hatudou2.jpg");
-  }
+  } else if (random === 2) {
+  console.log("シャウト");
+  $("div").children("img").attr("src", "image/hatudou3.jpg");
+}
 });
 
 // 音声認識パート
@@ -40,7 +43,8 @@ recognition.onresult = (event) => {
     }
     }
     resultDiv.innerHTML = finalTranscript + '<i style="color:#ddd;">' + interimTranscript + '</i>' ;
-}
+
+  }
 
 startBtn.onclick = () => {
     recognition.start();
@@ -49,64 +53,51 @@ stopBtn.onclick = () => {
     recognition.stop();
 } 
 
-const jugon = document.querySelector('#text');
-('#text').document('change', function(){
-  alert("入った")
+$("#stop-btn").on("click", function(){
+  console.log("ここから言葉判定")
+  const jugon = finalTranscript
+  console.log("言葉取れた")
+  if (jugon === "おかか") {
+    $("img").attr("src", "image/01okaka.jpg")
+  } else if (jugon === "鮭" ){
+    $("img").attr("src", "image/02shake.jpeg")
+  } else if (jugon === "いくら" ){
+    $("img").attr("src", "image/03ikura.jpg")
+  } else if (jugon === "筋子" ){
+    $("img").attr("src", "image/04sujiko.jpg")
+  } else if (jugon === "明太子" ){
+    $("img").attr("src", "image/05mentaiko.jpg")
+  } else if (jugon === "昆布" ){
+    $("img").attr("src", "image/06konbu.jpg")
+  } else if (jugon === "高菜" ){
+    $("img").attr("src", "image/07takana.png")
+  } else if (jugon === "ツナマヨ" ){
+    $("img").attr("src", "image/08tunamayo.jpg")
+  } else if (jugon === "ぶっ飛べ" ){
+    $("img").attr("src", "image/buttobe.jpg")
+  } else if (jugon === "デプロイ" ){
+    $("img").attr("src", "image/deploy.jpg")
+  } else if (jugon === "爆ぜろ" ){
+    $("img").attr("src", "image/hazero.jpg")
+  } else if (jugon === "眠れ" ){
+    $("img").attr("src", "image/nemure.jpg")
+  } else if (jugon === "逃げろ" ){
+    $("img").attr("src", "image/nigero.jpg")
+  } else if (jugon === "落ちろ" ){
+    $("img").attr("src", "image/ochiro.jpg")
+  } else if (jugon === "止まれ" ){
+    $("img").attr("src", "image/tomare.jpg")
+  } else if (jugon === "潰れろ" ){
+    $("img").attr("src", "image/tuburero.jpg")
+  } else if (jugon === "動くな" ){
+    $("img").attr("src", "image/ugokuna.png")
+  } else if (jugon === "貴重なご意見" ){
+    $("img").attr("src", "image/goiken.jpg")
+  } else {
+    $("img").attr("src", "image/genkai.png")
+  }
 });
 
-$("#result-div").onresult = () => {
-  console.log("aaa");
-};
-
-
-
-// if(document)($("#result-div")).match("鮭"){
-//   console.log("aaa");
-//   $("div").children("img").attr("src", "image/batu.jpg");
-//   console.log("bbb")
-// }
-
-
-// 背景画像変更
-$("#result-div").on("click", function () {
-    const random = Math.floor(Math.random() * 5);
-    console.log(random, "ランダム数字");
-
-    if (random === 0) {
-      console.log("空");
-      $("h2").text("たかくて　あおい　そら！").css("color", "black");
-      $(".randomhaikei").css("background-image", "url(image/aozora.jpg");
-    } else if (random === 1) {
-      console.log("機械工場");
-      $("h2").text("ここは　マシンが　いっぱいの　こうじょうだ！").css("color", "black");
-      $(".randomhaikei").css("background-image", "url(image/kikai.jpg");
-    } else if (random === 2) {
-      console.log("宇宙");
-      $("h2").text("ええっ！　うちゅうまで　きちゃった！").css("color", "black");
-      $(".randomhaikei").css("background-image", "url(image/space.jpg");
-    } else if (random === 3) {
-      console.log("砂漠");
-      $("h2").text("じりじり　あつい！　ここは　さばくだ！").css("color", "black");
-      $(".randomhaikei").css("background-image", "url(image/sabaku.png");
-    } else if (random === 4) {
-      console.log("岩山");
-      $("h2").text("ごつごつ。いわが　いっぱいの　やまだね！").css("color", "black");
-      $(".randomhaikei").css("background-image", "url(image/rock.jpg");
-    }
-  });
-
-// 最後にメッセージ
-  $(".btny").on("click", function(){
-    console.log("aaa");
-    $("h3").text("とっても　じょうずに　できたね！");
-    console.log("bbb")
-  });
-  
-  $(".btnn").on("click", function(){
-    console.log("aaa");
-    $("h3").text("とっても　すてきだよ！　じしんを　もってね！");
-    console.log("bbb")
-  });
 
 
 // $("xxxx") = 要素 class名 id名
